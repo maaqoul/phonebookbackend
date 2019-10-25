4b04b8809b41b9c5c70fa4e6a3f406891759d8da
maaqoul <hicham.maaqoul@gmail.com>
2019-10-25 11:29:44 +0100

    add cors module

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const cors = require("cors");
 morgan.token("body", (req, res) => {
   return JSON.stringify(req.body);
 });
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use(
